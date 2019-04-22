f993690f06eae3478f60599c9555d13797604c76
auditt98 <vietanh8i1998@gmail.com>
2019-04-22 15:25:18 +0700

    Added some view and controller for index and Teacher adding

---
+++ TMS/TeacherManagementSystem/TeacherManagementSystem/Controllers/GiaoVienController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeacherManagementSystem.Controllers
+{
+    public class GiaoVienController : Controller
+    {
+
+        //GET: GiaoVien/Index
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        // GET: GiaoVien/Them
+        public ActionResult Them()
+        {
+            return View();
+        }
+    }
+}
